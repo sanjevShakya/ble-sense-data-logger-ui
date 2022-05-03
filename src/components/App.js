e7cfc89a8e7b80985711eedb6a856ef5525eaaf3
Sanjeev Shakya <sanjevsakya@gmail.com>
2022-05-03 08:02:13 +0700

    gait detection fe

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import "./registerChart";
 import Routes from "./Routes";
 import SocketProvider from "./common/SocketProvider";
 import { BrowserRouter } from "react-router-dom";
+import '../assets/css/app.css';
 
 function App() {
   return (
--- src/components/inference/GaitDetection.js
+++ src/components/inference/GaitDetection.js
@@ -7,6 +7,12 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
+import spasticGif from "../../assets/spastic.gif";
+import walkingGif from "../../assets/walking.gif";
+import standingGif from "../../assets/standing.gif";
+import waddlingGif from "../../assets/waddling.gif";
+import propulsiveGif from "../../assets/propulsive.gif";
+
 export const SOCKET_EVENTS = {
   INFERENCE_START: "INFERENCE_START",
   INFERENCE_STARTED: "INFERENCE_STARTED",
@@ -21,6 +27,43 @@ const ACTIVITY_GAIT_CLASSES_MAP = Object.keys(GAIT_CLASSES)
     return acc;
   }, {});
 
+function GaitGif(props) {
+  const { gaitActivity } = props;
+
+  switch (gaitActivity) {
+    case GAIT_CLASSES.noMovement.name:
+      return (
+        <img className="gait-gif" src={standingGif} alt="Standing Gif"></img>
+      );
+    case GAIT_CLASSES.normalGait.name:
+      return (
+        <img className="gait-gif" src={walkingGif} alt="Walking Gif"></img>
+      );
+    case GAIT_CLASSES.waddlingGait.name:
+      return (
+        <img className="gait-gif" src={waddlingGif} alt="Waddling Gif"></img>
+      );
+    case GAIT_CLASSES.propulsiveGait.name:
+      return (
+        <img
+          className="gait-gif"
+          src={propulsiveGif}
+          alt="Propulsive Gif"
+        ></img>
+      );
+    case GAIT_CLASSES.spasticGait.name:
+      return (
+        <img className="gait-gif" src={spasticGif} alt="Spastic Gif"></img>
+      );
+    default:
+      return (
+        <img className="gait-gif" src={standingGif} alt="Standing Gif"></img>
+      );
+
+    // return null;
+  }
+}
+
 function GaitDetection(props) {
   const { socket } = props;
   const [inferenceStatus, updateInferenceStatus] = useState("null");
@@ -31,7 +74,7 @@ function GaitDetection(props) {
     socket.on(SOCKET_EVENTS.INFERENCE_RESULT, inferenceStatusHandler);
 
     return () => {
-      socket.off(SOCKET_EVENTS.INFERENCE_STOP);
+      socket.emit(SOCKET_EVENTS.INFERENCE_STOP);
     };
   }, [socket]);
 
@@ -50,10 +93,15 @@ function GaitDetection(props) {
             </Typography>
 
             {gaitState != undefined && (
-              <Typography component="div" variant="h6">
-                The person has{" "}
-                {ACTIVITY_GAIT_CLASSES_MAP[gaitState].displayName}.
-              </Typography>
+              <div>
+                <Typography component="div" variant="h6">
+                  The person has{" "}
+                  {ACTIVITY_GAIT_CLASSES_MAP[gaitState].displayName}.
+                </Typography>
+                <GaitGif
+                  gaitActivity={ACTIVITY_GAIT_CLASSES_MAP[gaitState].name}
+                ></GaitGif>
+              </div>
             )}
           </CardContent>
         </Card>
